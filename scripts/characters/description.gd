102820df6a0f71c469563073a3fbf496704dbd82
AricTriton <arictriton@gmail.com>
2022-02-07 23:44:43 -0500

    Small tweaks & fixes

--- scripts/characters/description.gd
+++ scripts/characters/description.gd
@@ -96,7 +96,7 @@ func onceperdayConvos():
 
 func showVice():
 	var text = ""
-	if person.mind.vice_known == false:
+	if person.mind.vice_known == false || globals.expansionsettings.vices_luxury_effects == false:
 		return ""
 	match person.mind.vice:
 		'lust':
