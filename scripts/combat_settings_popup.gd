a9e7fbcda3614647b19ea7a6ed68bb9beea6850a
zclimber <zclimber@example.com>
2023-03-05 20:57:15 +0100

    Add combat_settings_popup to mansion

---
+++ scripts/combat_settings_popup.gd
@@ -0,0 +1,303 @@
+extends Popup
+
+# Meta variable names
+const META_CONTAINER_NAME = "container_name"
+const META_INDEX = "container_index"
+const META_HINT_TEXT = "hint_text"
+
+# META_CONTAINER_NAME values 
+const ALL_SKILLS_NAME = "all_skills"
+const ACTIVE_SKILLS_NAME = "active_skills"
+const AUTO_SKILLS_NAME = "auto_skills"
+
+
+var trash_texture = load("res://files/aric_expansion_images/skill_icons/TrashCan.png")
+var add_skill_texture = load("res://files/aric_expansion_images/skill_icons/AddSkill.png")
+
+
+# Container with all_skills buttons
+var all_skills_container: GridContainer
+
+# Buttons for skills in all_skills
+var all_skills_buttons: Array
+
+# Container with active_skills buttons
+var active_skills_container: GridContainer
+
+# Buttons for skills in active_skills
+var active_skills_buttons: Array
+
+# Buttons (currently 1) for add and delete actions in active_skills
+var active_skills_add_buttons: Array
+
+# Container with auto_skills buttons
+var auto_skills_container: GridContainer
+
+# Buttons for skills in auto_skills
+var auto_skills_buttons: Array
+
+# Buttons (currently 1) for add and delete actions in auto_skills
+var auto_skills_add_buttons: Array
+
+# All skills person knows
+var all_skills = []
+
+# Skills that person will have in fight
+var active_skills = []
+
+# Skills that person will use for auto-attack
+var auto_skills = []
+
+# Current person whose skills are being changed
+var current_person
+
+# Skills present on opening edit, to use for discarding
+var old_all_skills
+var old_active_skills
+var old_auto_skills
+
+
+# Ensure container has at least visible_count visible buttons and invisible_count invisible buttons
+# Return [array of visible buttons, array of invisible buttons]
+func ensure_buttons(container: GridContainer, visible_count: int, invisible_count: int, container_name: String) -> Array:
+	var original_button = container.get_node("original_button")
+	var total_count = invisible_count + visible_count
+	var buttons = []
+
+	for button in container.get_children():
+		if button == original_button:
+			continue		
+		button.visible = buttons.size() < visible_count
+		if buttons.size() < total_count:
+			buttons.append(button)
+	
+	while buttons.size() < total_count:
+		var button = create_new_button(container, original_button, container_name, buttons.size())
+		button.visible = buttons.size() < visible_count
+		buttons.append(button)
+	
+	var visible_buttons = []
+	if visible_count > 0:
+		visible_buttons = buttons.slice(0, visible_count - 1)
+	var invisible_buttons = []
+	if invisible_count > 0:
+		invisible_buttons = buttons.slice(visible_count, total_count - 1)
+	
+	return [visible_buttons, invisible_buttons]
+
+
+func set_skill_textures(buttons: Array, abilities: Array) -> void:
+	for index in buttons.size():
+		var button = buttons[index]
+		var ability = abilities[index]
+		button.texture_normal = ability.iconnorm
+		var hint_text = '[center]%s[/center]\n\n%s' % [ability.name, ability.description]
+		button.set_meta(META_HINT_TEXT, hint_text)
+
+
+func show_ability_tooltip(button: TextureButton) -> void:
+	if button.has_meta(META_HINT_TEXT):
+		var tooltip = button.get_meta(META_HINT_TEXT)
+		globals.showtooltip(tooltip)
+
+
+func hide_tooltip() -> void:
+	globals.hidetooltip()
+
+
+func create_new_button(container: GridContainer, original_button: TextureButton,
+					   container_name: String, index: int) -> TextureButton:
+	var new_button = original_button.duplicate()
+	container.add_child(new_button)
+	new_button.set_drag_forwarding(self)
+	new_button.set_meta(META_CONTAINER_NAME, container_name)
+	new_button.set_meta(META_INDEX, index)
+	new_button.connect("mouse_entered",self,'show_ability_tooltip',[new_button])
+	new_button.connect("mouse_exited",self,'hide_tooltip')
+	return new_button
+
+
+static func load_skills_from_names(ability_list: Array) -> Array:
+	var result = []
+	for skill_id in ability_list:
+		result.append(globals.abilities.abilitydict[skill_id])
+	return result
+
+
+static func check_all_skills_learned(all_skills: Array, checked_skills: Array) -> void:
+	var index = 0
+	while index < checked_skills.size():
+		if all_skills.has(checked_skills[index]):
+			index += 1
+		else:
+			checked_skills.remove(index)
+
+
+func enforce_ability_constraints() -> void:
+	check_all_skills_learned(all_skills, active_skills)
+	check_all_skills_learned(all_skills, auto_skills)
+
+	for index in range(auto_skills.size() - 1, -1, -1):
+		if auto_skills[index].targetgroup != "enemy":
+			auto_skills.remove(index)
+	
+	var attack_skill = globals.abilities.abilitydict["attack"]
+	if not auto_skills.has(attack_skill):
+		auto_skills.append(attack_skill)
+
+
+# Ensure each ability list has all current abilities with proper icons, and add buttons also have proper icons
+func recalc_buttons() -> void:
+	enforce_ability_constraints()
+	
+	current_person.ability = get_ability_names_array(all_skills)
+	current_person.abilityactive = get_ability_names_array(active_skills)
+	current_person.ability_autoattack = get_ability_names_array(auto_skills)
+
+	all_skills_buttons = ensure_buttons(all_skills_container, all_skills.size(), 0, ALL_SKILLS_NAME)[0]
+	var active_skills_buttons_pair = ensure_buttons(active_skills_container, active_skills.size(), 1, ACTIVE_SKILLS_NAME)
+	var auto_skills_buttons_pair = ensure_buttons(auto_skills_container, auto_skills.size(), 1, AUTO_SKILLS_NAME)
+
+	active_skills_buttons = active_skills_buttons_pair[0]
+	auto_skills_buttons = auto_skills_buttons_pair[0]
+
+	active_skills_add_buttons = active_skills_buttons_pair[1]
+	auto_skills_add_buttons = auto_skills_buttons_pair[1]
+
+	set_skill_textures(all_skills_buttons, all_skills)
+	set_skill_textures(active_skills_buttons, active_skills)
+	set_skill_textures(auto_skills_buttons, auto_skills)
+
+
+func move_data(from_container: String, from_index: int, to_container: String, to_index: int) -> void:
+	var container_skills = {ALL_SKILLS_NAME: all_skills, ACTIVE_SKILLS_NAME: active_skills, AUTO_SKILLS_NAME: auto_skills}
+
+	if from_container == to_container:
+		move_ability(container_skills[from_container], from_index, to_index)
+	elif from_container == ALL_SKILLS_NAME:
+		add_new_ability(from_index, container_skills[to_container], to_index)
+	
+	recalc_buttons()
+
+
+func add_new_ability(from_index: int, to_ability_array: Array, to_index: int) -> void:
+	var added_ability = all_skills[from_index]
+	if to_ability_array.has(added_ability):
+		from_index = to_ability_array.find(added_ability)
+		if to_index > from_index:
+			to_index -= 1
+		move_ability(to_ability_array, from_index, to_index)
+	elif to_index < to_ability_array.size():
+		to_ability_array.insert(to_index, added_ability)
+	else:
+		to_ability_array.push_back(added_ability)
+
+
+func move_ability(ability_array: Array, from_index: int, to_index: int) -> void:
+	if from_index == to_index:
+		return
+	var moved_ability = ability_array.pop_at(from_index)
+	if to_index <= ability_array.size():
+		ability_array.insert(to_index, moved_ability)	
+
+
+func on_move_from(from_container: String, from_index: int) -> void:
+	if from_container == ALL_SKILLS_NAME:
+		active_skills_add_buttons[0].texture_normal = add_skill_texture
+		active_skills_add_buttons[0].visible = true
+		auto_skills_add_buttons[0].texture_normal = add_skill_texture
+		auto_skills_add_buttons[0].visible = true
+
+	if from_container == ACTIVE_SKILLS_NAME and from_index <= active_skills.size():
+		active_skills_add_buttons[0].texture_normal = trash_texture
+		active_skills_add_buttons[0].visible = true
+
+	if from_container == AUTO_SKILLS_NAME and from_index <= auto_skills.size():
+		auto_skills_add_buttons[0].texture_normal = trash_texture
+		auto_skills_add_buttons[0].visible = true
+
+
+func control_has_meta(control: Control) -> bool:
+	return control.has_meta(META_CONTAINER_NAME) and control.has_meta(META_INDEX)
+
+
+func can_drop_data_fw(position, data, from_control) -> bool:
+	if typeof(data) != TYPE_DICTIONARY or not data.has_all([META_CONTAINER_NAME, META_INDEX]):
+		return false
+	if !control_has_meta(from_control):
+		return false
+	var from_container = data[META_CONTAINER_NAME]
+	var to_container = from_control.get_meta(META_CONTAINER_NAME)
+	return from_container == ALL_SKILLS_NAME or from_container == to_container
+
+
+func drop_data_fw(position, data, from_control) -> void:
+	if not can_drop_data_fw(position, data, from_control):
+		return
+	move_data(data[META_CONTAINER_NAME], data[META_INDEX],
+			  from_control.get_meta(META_CONTAINER_NAME), from_control.get_meta(META_INDEX))
+
+
+func get_drag_data_fw(position, from_control : TextureButton):
+	if not control_has_meta(from_control):
+		return null
+	var container_name = from_control.get_meta(META_CONTAINER_NAME)
+	var index = from_control.get_meta(META_INDEX)
+	
+	var preview_object = from_control.duplicate()
+	preview_object.connect("tree_exiting", self, "hide_add_buttons")
+	set_drag_preview(preview_object)
+	on_move_from(container_name, index)
+	hide_tooltip()
+	return {META_CONTAINER_NAME: container_name, META_INDEX: index}
+
+
+func hide_add_buttons() -> void:
+	for button in active_skills_add_buttons:
+		button.visible = false
+	for button in auto_skills_add_buttons:
+		button.visible = false
+
+
+func get_ability_names_array(abilities_array: Array) -> Array:
+	var names_array = []
+	for ability in abilities_array:
+		names_array.append(ability.code)
+	return names_array
+
+
+func set_person(person) -> void:
+	current_person = person
+	
+	old_all_skills = person.ability
+	old_active_skills = person.abilityactive
+	old_auto_skills = person.ability_autoattack
+	
+	all_skills = load_skills_from_names(person.ability)
+	active_skills = load_skills_from_names(person.abilityactive)
+	auto_skills = load_skills_from_names(person.ability_autoattack)
+	
+	recalc_buttons()
+
+
+func _on_save_button_pressed() -> void:
+	hide()
+
+
+func _on_reset_button_pressed() -> void:
+	active_skills = all_skills
+	auto_skills = []
+	recalc_buttons()
+
+
+func _on_discard_button_pressed() -> void:
+	current_person.ability = old_all_skills
+	current_person.abilityactive = old_active_skills
+	current_person.ability_autoattack = old_auto_skills
+	hide()
+
+
+func _ready() -> void:
+	all_skills_container = get_node("panel/all_skills/scroll/container")
+	active_skills_container = get_node("panel/active_skills/scroll/container")
+	auto_skills_container = get_node("panel/auto_skills/scroll/container")
--- scripts/groupselectpanel.gd
+++ scripts/groupselectpanel.gd
@@ -1,30 +1,80 @@
+extends Panel
 
-func show():
-	var newbutton
-	var group
-	var text = ''
+# Store pair of [person, button] for each slave not away, to choose for party 
+var group_select_persons_buttons: Array = []
+
+# Store MAX_PARTY_SIZE buttons for changing combat prefs of party members
+var combat_settings_buttons: Array = []
+
+# Store MAX_PARTY_SIZE persons in combat party, their combat prefs can be changed
+var combat_settings_persons: Array = []
+
+const MAX_PARTY_SIZE = 4
+
+
+func show() -> void:
 	get_parent().checkplayergroup()
 	get_parent()._on_mansion_pressed()
 	if OS.get_name() != 'HTML5' && globals.rules.fadinganimation == true:
 		yield(get_parent(), 'animfinished')
 	self.visible = true
-	for i in get_node("grouppanel/ScrollContainer/VBoxContainer").get_children():
-		if i != get_node("grouppanel/ScrollContainer/VBoxContainer/Button"):
-			i.visible = false
-			i.queue_free()
-	for person in globals.slaves:
+	recreate_group_selection()
+	recreate_combat_settings()
+
+
+func recreate_group_selection() -> void:	
+	var scroll_node = get_node("grouppanel/slave_list_scroll/vbox")
+	group_select_persons_buttons = update_list(scroll_node, globals.slaves)
+	for person_button in group_select_persons_buttons:
+		var newbutton = person_button[1]
+		newbutton.connect("pressed", self, 'addtogroup', person_button)
+
+	update_group_selection()
+
+
+func update_group_selection() -> void:
+	update_group_selection_buttons()
+	update_group_tooltip()
+
+
+# Deletes old and creates new buttons for each person in array persons, returns array of pairs [person, button]
+static func update_list(vbox_container: Node, persons: Array) -> Array:
+	var original_button = vbox_container.get_node("original_button")
+
+	# Remove all buttons not equal to original_button from vbox_container 
+	for button in vbox_container.get_children():
+		if button != original_button:
+			button.visible = false
+			button.queue_free()
+	# Create new buttons for each slave and player, then call "self.new_button_callback" on each
+	var added_buttons = []
+	for person in persons:
 		if person.away.at == 'hidden':
 			continue
-		newbutton = get_node("grouppanel/ScrollContainer/VBoxContainer/Button").duplicate()
-		get_node("grouppanel/ScrollContainer/VBoxContainer").add_child(newbutton)
-		newbutton.visible = true
+		var newbutton = create_new_button(original_button, vbox_container)
+		added_buttons.append([person, newbutton])
+	return added_buttons
+
+
+# Create new button and return it
+static func create_new_button(original_button: Node, vbox_container: Node) -> Node:
+	var newbutton = original_button.duplicate()
+	vbox_container.add_child(newbutton)
+	newbutton.visible = true
+	return newbutton
+
+
+# Update name, tooltip and state for adding to group buttons
+func update_group_selection_buttons() -> void:
+	for person_button in group_select_persons_buttons:
+		var person = person_button[0]
+		var newbutton = person_button[1]
 		newbutton.set_text(person.name_long() + ' ' + person.race)
 		if globals.state.playergroup.has(str(person.id)):
+			newbutton.set_disabled(false)
 			newbutton.set_pressed(true)
 		###---Added by Expansion---### Join Consent
-#		elif globals.state.playergroup.size() >= 3 || person.energy <= 10 || person.stress >= 80 || person.loyal + person.obed < 80 || person.sleep == 'jail' || person.away.duration != 0 || person.consentexp.party == false && person.unique == null || person.movement == 'none':
-#			newbutton.set_disabled(true)
-		elif globals.state.playergroup.size() >= 3:
+		elif globals.state.playergroup.size() + 1 >= MAX_PARTY_SIZE:
 			newbutton.set_disabled(true)
 			newbutton.set_tooltip(person.dictionary('You have too many people in your party for $name to join.'))
 		elif person.energy <= 10:
@@ -45,16 +95,74 @@ func show():
 		elif person.movement == 'none':
 			newbutton.set_disabled(true)
 			newbutton.set_tooltip(person.dictionary('$name cannot move right now, much less travel.'))
+		else:
+			newbutton.set_disabled(false)
+			newbutton.set_pressed(false)
 		###---End Expansion---###
-		newbutton.connect("pressed",self,'addtogroup',[person, newbutton])
+	update_group_tooltip()
+
+
+#Update label with group list
+func update_group_tooltip() -> void:
+	var text = ''
 	if globals.state.playergroup.size() <= 0:
 		text = 'You have no assigned followers'
 	else:
 		text = 'You will be accompanied by:\n'
 	for i in globals.state.playergroup:
-		group = globals.state.findslave(i)
-		text = text + group.name_long() + ', ' + group.race +', Level: ' +  str(group.level) + ', Health: '+str(round(group.health)) + ", Energy: "+ str(round(group.energy))+  '\n'
+		var person = globals.state.findslave(i)
+		text = text + '%s, %s, Level: %s, Health: %s, Energy: %s\n' % [person.name_long(), person.race, person.level, round(person.health), round(person.energy)]
 	get_node("grouppanel/grouplabel").set_bbcode(text)
-	#updateitemsinventory()
-	#updateitemsbackpack()
-	#calculateweight()
+
+
+func addtogroup(person, button) -> void:
+	if button.is_pressed() == true:
+		globals.state.playergroup.append(person.id)
+	else:
+		globals.state.playergroup.remove(globals.state.playergroup.find(person.id))
+	update_group_selection()
+	update_combat_settings_buttons()
+
+
+func update_combat_settings_buttons() -> void:
+	combat_settings_persons = [globals.player]
+	for slave_id in globals.state.playergroup:
+		combat_settings_persons.append(globals.state.findslave(slave_id))
+	
+	for index in MAX_PARTY_SIZE:
+		var button = combat_settings_buttons[index]
+		if index >= combat_settings_persons.size():
+			button.visible = false
+			continue
+		var person = combat_settings_persons[index]
+		button.set_text(person.name_long() + ' ' + person.race)
+		button.visible = true
+
+
+func recreate_combat_settings() -> void:
+	var vbox_container = get_node("grouppanel/combat_settings_scroll/vbox")
+
+	var max_party_array = []
+	max_party_array.resize(MAX_PARTY_SIZE)
+	max_party_array.fill(globals.player)
+	var combat_settings_persons_buttons = update_list(vbox_container, max_party_array)
+
+	combat_settings_buttons = []
+	for index in MAX_PARTY_SIZE:
+		var newbutton = combat_settings_persons_buttons[index][1]
+		newbutton.connect("pressed", self, 'show_combat_settings_for_index', [index])
+		combat_settings_buttons.append(newbutton)
+	
+	update_combat_settings_buttons()
+
+
+func show_combat_settings_for_index(index: int) -> void:
+	var person = combat_settings_persons[index]
+	var node = get_node("grouppanel/combat_settings_popup")
+	node.set_person(person)
+	node.popup()
+
+
+func _on_closegroup_pressed() -> void:
+	self.visible = false
+	get_parent()._on_mansion_pressed()
