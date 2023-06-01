5af508348bda824175a5ca706036c4ec5d216e2b
zclimber <zclimber@example.com>
2023-06-02 00:13:15 +0200

    Fix wrong merge and remove more spaces

--- scripts/statspanel.gd
+++ scripts/statspanel.gd
@@ -3,28 +3,31 @@ func _ready():
 	for i in globals.statsdict:
 		get(i).get_parent().get_node("Control").connect("mouse_entered", self, 'showtooltip', [i])
 		get(i).get_parent().get_node("Control").connect("mouse_exited", globals, 'hidetooltip')
-	
+
 
 func showtooltip(value):
 	var text = globals.statsdescript[value]
 	globals.showtooltip(text)
+
 
 func statup(stat):
 	person[stat] += 1
 	person.skillpoints -= 1
 	show()
 
+
 static func add_color_tag(text, color: String) -> String:
 	return "[color=%s]%s[/color]" % [color, text]
+
 
 func show():
 	var mode_adv = mode in ['full','slaveadv']
 	var mode_full = mode in ['full']
-  
+
 	show_stats(mode_adv, mode_full)
 	show_traits(mode_adv, mode_full)
 	show_stats_text(mode_adv, mode_full)
-  get_node("levelprogress/Label").set_text("Experience: %d/%d (%d%%)" % [person.not_percent_xp, person.not_percent_max_xp, person.xp])
+
 
 func show_stats(mode_adv: bool, mode_full: bool) -> void:
 	for i in globals.statsdict:
@@ -34,7 +37,7 @@ func show_stats(mode_adv: bool, mode_full: bool) -> void:
 				stat_text = add_color_tag(stat_text, "green")
 			elif person.stats[globals.maxstatdict[i].replace("_max",'_mod')] < 0:
 				stat_text = add_color_tag(stat_text, "red")
-		
+
 		if mode_adv:
 			stat_text += "/" + str(min(person.stats[globals.maxstatdict[i]], person.originvalue[person.origins]))
 		get(i).set_bbcode(stat_text)
@@ -57,7 +60,7 @@ func show_traits(mode_adv: bool, mode_full: bool) -> void:
 			elif i.tags.find('detrimental') >= 0:
 				trait_text = add_color_tag(trait_text, "#ff4949")
 			traits_array.append(trait_text)
-		
+
 		traits_text += "$name has trait(s): %s." % traits_array.join(", ")
 
 	if person.spec != null:
@@ -83,10 +86,10 @@ func show_stats_text(mode_adv: bool, mode_full: bool) -> void:
 		obedience_string = add_color_tag("Rebellious", "#ff4949")
 	elif person.obed < 40:
 		obedience_string = add_color_tag("Obeys poorly", "#ff4949")
-	
+
 	if mode_adv:
 		level_data.append("Obedience: %d (%s)" % [round(person.obed), obedience_string])
 	else:
 		level_data.append("Obedience: %s" % obedience_string)
-	
+
 	get_node("text_container/leveltext").set_bbcode(person.dictionary(level_data.join("\n")))
